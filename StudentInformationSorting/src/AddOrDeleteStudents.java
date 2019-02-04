c1d3592e49da1b3f61c49d365b265662c9500ad7
robert.kulbacki <robert.kulbacki@mu-c1-03.mullen.pvt.k12.co.us>
2019-02-04 08:41:27 -0700

    new branch add..

---
+++ StudentInformationSorting/src/AddOrDeleteStudents.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class AddOrDeleteStudents
+	{
+		
+		public static void printMenu()
+		{
+			Scanner userInput1 = new Scanner(System.in);
+			System.out.println("1) Add a student.");
+			System.out.println("2) Delete a student.");
+			int choice = userInput1.nextInt();
+			
+			if(choice==1)
+				{
+					System.out.println("Which student would you like to delete?");
+					//print students
+					//delete student
+				}
+			else 
+				{
+					//add student 
+				}
+		}
+	}
