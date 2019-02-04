40c3f044a0fefa1237ced128ec90c6d9003f5b1b
amelia.brown <amelia.brown@MU-C1-09.mullen.pvt.k12.co.us>
2019-02-04 12:23:39 -0700

    added text file and started reading file

--- StudentInformationSorting/src/SISRunner.java
+++ StudentInformationSorting/src/SISRunner.java
@@ -1,11 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class SISRunner
 	{
 
-		public static void main(String[] args)
+		public static void main(String[] args) throws IOException
 			{
 				System.out.println("hello");
 
+				Scanner names = new Scanner (new File ("studentList"));
+				while(names.hasNext())
+				{
+					System.out.println(names.next());
+				}
+				
 			}
 
 	}
