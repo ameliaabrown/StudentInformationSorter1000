21472b81e7c0c9e2aa1cc450881c6ff954c67cfb
robert.kulbacki <robert.kulbacki@mu-c1-03.mullen.pvt.k12.co.us>
2019-02-05 08:13:47 -0700

    created student arraylist

--- StudentInformationSorting/src/SISRunner.java
+++ StudentInformationSorting/src/SISRunner.java
@@ -2,11 +2,13 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SISRunner
 	{
-
+		static ArrayList<Student> fullClass = new ArrayList<Student>();
+		
 		public static void main(String[] args) throws IOException
 			{
 				//System.out.println("hello");
@@ -14,6 +16,9 @@ public class SISRunner
 				Scanner names = new Scanner (new File ("studentList"));
 				while(names.hasNext())
 				{
+					String first = names.next();
+					String last = names.next();
+					fullClass.add(new Student(first, last, , ));
 					System.out.println(names.next());
 				}
 				
